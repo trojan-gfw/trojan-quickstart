cd2b1d5cd2e20653f28bce560440c9029f204fd8
GreaterFire <32649575+GreaterFire@users.noreply.github.com>
2019-07-30 07:54:08 -0700

    General improvements

--- trojan-quickstart.sh
+++ trojan-quickstart.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-set -e
+set -euo pipefail
 
 if [[ $(id -u) != 0 ]]; then
     echo Please run this script as root.
@@ -9,31 +8,35 @@ fi
 
 NAME=trojan
 VERSION=1.12.3
-TARBALL=$NAME-$VERSION-linux-amd64.tar.xz
-DOWNLOADURL=https://github.com/trojan-gfw/$NAME/releases/download/v$VERSION/$TARBALL
-TMPDIR=$(mktemp -d)
+TARBALL="$NAME-$VERSION-linux-amd64.tar.xz"
+DOWNLOADURL="https://github.com/trojan-gfw/$NAME/releases/download/v$VERSION/$TARBALL"
+TMPDIR="$(mktemp -d)"
 INSTALLPREFIX=/usr/local
 SYSTEMDPREFIX=/etc/systemd/system
 
+BINARYPATH="$INSTALLPREFIX/bin/$NAME"
+CONFIGPATH="$INSTALLPREFIX/etc/$NAME/config.json"
+SYSTEMDPATH="$SYSTEMDPREFIX/$NAME.service"
+
 echo Entering temp directory $TMPDIR...
-cd $TMPDIR
+cd "$TMPDIR"
 
 echo Downloading $NAME $VERSION...
-curl -LO $DOWNLOADURL || wget $DOWNLOADURL
+curl -LO "$DOWNLOADURL" || wget "$DOWNLOADURL"
 
 echo Unpacking $NAME $VERSION...
-tar xf $TARBALL
-cd $NAME
+tar xf "$TARBALL"
+cd "$NAME"
 
-echo Installing $NAME $VERSION to $INSTALLPREFIX/bin...
-install -Dm755 $NAME $INSTALLPREFIX/bin/$NAME
+echo Installing $NAME $VERSION to $BINARYPATH...
+install -Dm755 "$NAME" "$BINARYPATH"
 
-echo Installing $NAME server config to $INSTALLPREFIX/etc/$NAME...
-install -Dm644 examples/server.json-example $INSTALLPREFIX/etc/$NAME/config.json
+echo Installing $NAME server config to $CONFIGPATH...
+install -Dm644 examples/server.json-example "$CONFIGPATH"
 
-if [[ -e $SYSTEMDPREFIX ]]; then
-    echo Installing $NAME systemd service to $SYSTEMDPREFIX...
-    cat > $NAME.service << EOF
+if [[ -e "$SYSTEMDPREFIX" ]]; then
+    echo Installing $NAME systemd service to $SYSTEMDPATH...
+    cat > "$SYSTEMDPATH" << EOF
 [Unit]
 Description=trojan
 Documentation=https://trojan-gfw.github.io/trojan/config https://trojan-gfw.github.io/trojan/
@@ -42,19 +45,18 @@ After=network.target network-online.target nss-lookup.target mysql.service maria
 [Service]
 Type=simple
 StandardError=journal
-ExecStart=/usr/local/bin/trojan /usr/local/etc/trojan/config.json
-ExecReload=/bin/kill -HUP $MAINPID
+ExecStart="$BINARYPATH" "$CONFIGPATH"
+ExecReload=/bin/kill -HUP \$MAINPID
 
 [Install]
 WantedBy=multi-user.target
 EOF
-    install -Dm644 $NAME.service $SYSTEMDPREFIX/$NAME.service
 
     echo Reloading systemd daemon...
     systemctl daemon-reload
 fi
 
 echo Deleting temp directory $TMPDIR...
-rm -rf $TMPDIR
+rm -rf "$TMPDIR"
 
 echo Done!
