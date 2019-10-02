f8880fd79a01eb435fc75a1ae6c2751e69d58cfb
GreaterFire <32649575+GreaterFire@users.noreply.github.com>
2019-10-02 07:53:01 -0700

    Bump 1.13.0

--- trojan-quickstart.sh
+++ trojan-quickstart.sh
@@ -17,7 +17,7 @@ if [[ $(id -u) != 0 ]]; then
 fi
 
 NAME=trojan
-VERSION=1.12.3
+VERSION=1.13.0
 TARBALL="$NAME-$VERSION-linux-amd64.tar.xz"
 DOWNLOADURL="https://github.com/trojan-gfw/$NAME/releases/download/v$VERSION/$TARBALL"
 TMPDIR="$(mktemp -d)"
